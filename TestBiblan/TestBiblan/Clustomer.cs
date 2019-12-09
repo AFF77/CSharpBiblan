d37c4dd2b2284dc087cfdc266f91f425efe8a38d
AFF77 <56636954+AFF77@users.noreply.github.com>
2019-12-09 13:29:59 +0100

    customer
    klassen klart!

---
+++ TestBiblan/TestBiblan/Clustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBiblan
+{
+    class Clustomer
+    {
+        int count;
+        string name, last_name;
+        public Clustomer(int count, string name) {
+            this.count = count;
+            this.name = name;
+        }
+    }
+}
