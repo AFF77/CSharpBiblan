7d72f56e24ea8eaf6f55caa86da0cd386cebfcb3
AFF77 <56636954+AFF77@users.noreply.github.com>
2019-12-09 13:19:54 +0100

    Skapad projektet
    xjk.fdhgfdljlfdjhfd-lhjfh

---
+++ TestBiblan/TestBiblan/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBiblan
+{
+    class Admin
+    {
+        int age;
+        string name, last_name;
+    }
+}
